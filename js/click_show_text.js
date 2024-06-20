a8210c139e4ca9c9bbcf2a12ee42a55c3a1cce82
Cidream <509611667@qq.com>
2024-06-20 20:05:04 +0800

    Site updated: 2024-06-20 20:05:04

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     $("body").click(function (e) {
         let string_list = ["🔮AC and AK🔮","🔮二进制🔮", "🔮前缀和🔮", "🔮差分🔮", "🔮二分与三分🔮", "🔮倍增🔮", "🔮贪心🔮", "🔮搜索🔮", "🔮数论🔮", "🔮数据结构🔮", "🔮图论🔮", "🔮动态规划🔮",];
         let color_list = ["#ff3333", "#ff8000", "#f9f906", "#b9f20d", "#00ff00", "#00ff80", "#00ffff", "#007fff", "#0000ff", "#7f00ff", "#ff00ff", "#ff0080"];
-        let string = $("<span>").text(string_list[index]);
+        let string = $("<span/>").text(string_list[index]);
         let color = color_list[index];
         index = (index + 1) % string_list.length;
         let x = e.pageX, y = e.pageY;
@@ -12,4 +12,3 @@ jQuery(document).ready(function ($) {
         string.animate({ "top": y - 180, "opacity": 0 }, 1500, function () { string.remove(); });
     });
 });
-</span>
